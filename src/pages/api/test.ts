f1ed764ffb9924156974b2bebea75faef2bc7d94
LMsqz <luisfmasaquiza@hotmail.com>
2024-06-20 16:15:43 +0200

    configuracion

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,3 +1,5 @@
+import type { APIRoute } from "astro";
+
 export async function GET(){
     return new Response(
         JSON.stringify({msg: "Hellos w"}),{
@@ -8,9 +10,9 @@ export async function GET(){
         }
     )
 }
-export async function POST({}){
-    return new Response(
-        JSON.stringify({msg: "Hellos w"}),{
+export const POST: APIRoute = async ({params, request}) => {
+    const body = await request.json();
+    return new Response(JSON.stringify(body),{
             status: 200,
             headers: {
                 "Content-Type": "application/json"
